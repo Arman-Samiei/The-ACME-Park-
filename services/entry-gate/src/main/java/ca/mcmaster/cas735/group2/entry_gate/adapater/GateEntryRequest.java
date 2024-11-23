d39ef1fed24c8803ebb46d9c7ebcfd8c357e1328
Talha Asif <tasif@venacorp.com>
2024-11-23 18:08:26 -0500

    replace driver adapters with REST, create driven ports and adapters

---
+++ services/entry-gate/src/main/java/ca/mcmaster/cas735/group2/entry_gate/adapater/GateEntryRequest.java
@@ -0,0 +1,48 @@
+package ca.mcmaster.cas735.group2.entry_gate.adapater;
+
+import ca.mcmaster.cas735.group2.entry_gate.business.EntryGateService;
+import ca.mcmaster.cas735.group2.entry_gate.dto.TransponderGateActionDTO;
+import ca.mcmaster.cas735.group2.entry_gate.dto.VoucherGateActionDTO;
+import ca.mcmaster.cas735.group2.entry_gate.ports.TransponderGateActivity;
+import ca.mcmaster.cas735.group2.entry_gate.ports.VisitorGateActivity;
+import ca.mcmaster.cas735.group2.entry_gate.ports.VoucherGateActivity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/entry/request")
+public class GateEntryRequest implements TransponderGateActivity, VisitorGateActivity, VoucherGateActivity {
+
+    private final EntryGateService entryGateService;
+
+    @Autowired
+    public GateEntryRequest(EntryGateService entryGateService) {
+        this.entryGateService = entryGateService;
+    }
+
+    @PostMapping(value = "/transponder")
+    @Override
+    @ResponseStatus(HttpStatus.OK)
+    public void receiveTransponderGateActivity(@RequestBody TransponderGateActionDTO transponderGateActionDTO) {
+        entryGateService.validateAndProcessGateAction(transponderGateActionDTO);
+    }
+
+    @PostMapping(value = "/visitor")
+    @Override
+    @ResponseStatus(HttpStatus.OK)
+    public String receiveVisitorGateActivity() {
+        return entryGateService.validateAndProcessGateActionForVisitor();
+    }
+
+    @PostMapping(value = "/voucher")
+    @Override
+    @ResponseStatus(HttpStatus.OK)
+    public void receiveVoucherGateActivity(@RequestBody VoucherGateActionDTO voucherGateActionDTO) {
+        entryGateService.validateAndProcessGateAction(voucherGateActionDTO);
+    }
+}
--- services/entry-gate/src/main/java/ca/mcmaster/cas735/group2/entry_gate/business/EntryGateService.java
+++ services/entry-gate/src/main/java/ca/mcmaster/cas735/group2/entry_gate/business/EntryGateService.java
@@ -1,12 +1,11 @@
 package ca.mcmaster.cas735.group2.entry_gate.business;
 
 import ca.mcmaster.cas735.group2.entry_gate.dto.TransponderGateActionDTO;
-import ca.mcmaster.cas735.group2.entry_gate.dto.VisitorGateActionDTO;
 import ca.mcmaster.cas735.group2.entry_gate.dto.VoucherGateActionDTO;
 
-public interface ParkingService {
+public interface EntryGateService {
 
     void validateAndProcessGateAction(TransponderGateActionDTO transponderGateActionDTO);
-    void validateAndProcessGateAction(VisitorGateActionDTO visitorGateActionDTO);
     void validateAndProcessGateAction(VoucherGateActionDTO voucherGateActionDTO);
+    String validateAndProcessGateActionForVisitor();
 }
