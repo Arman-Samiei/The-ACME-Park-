66b54fdf9136bc2dd40559556019f3d52ee8bbfc
Arman-Samiei <samiei.arman@gmail.com>
2024-11-26 04:45:09 -0500

    Create a port for accessing the database

---
+++ services/permit/src/main/java/ca/mcmaster/cas735/group2/permit/ports/required/PermitRepository.java
@@ -0,0 +1,8 @@
+package ca.mcmaster.cas735.group2.permit.ports.required;
+
+import ca.mcmaster.cas735.group2.permit.business.entities.PermitData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermitRepository extends JpaRepository<PermitData, String> {
+    public PermitData findByTransponderID(String transponderID);
+}
