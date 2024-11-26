571f06efb0ae18dc38e69110d6c6fc6c658c38fd
Arman-Samiei <samiei.arman@gmail.com>
2024-11-26 04:41:50 -0500

    Create permit spring application

---
+++ services/permit/src/main/java/ca/mcmaster/cas735/group2/permit/PermitApplication.java
@@ -0,0 +1,12 @@
+package ca.mcmaster.cas735.group2.permit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermitApplication.class, args);
+    }
+}
