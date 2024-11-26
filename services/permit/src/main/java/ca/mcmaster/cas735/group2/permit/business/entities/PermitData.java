6d13e049b8a670cae42ce7d68d4cbdcd4c02ed12
Arman-Samiei <samiei.arman@gmail.com>
2024-11-26 04:43:15 -0500

    Create PermitData corresponding to the data persisted in the database

---
+++ services/permit/src/main/java/ca/mcmaster/cas735/group2/permit/business/entities/PermitData.java
@@ -0,0 +1,22 @@
+package ca.mcmaster.cas735.group2.permit.business.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity @Data public class PermitData {
+    @Id
+    private String transponderID;
+
+    private String PlateNumber;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String employeeID;
+
+    private String lotNumber;
+}
+
+
