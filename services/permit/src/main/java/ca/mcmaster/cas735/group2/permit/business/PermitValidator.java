9cab56eaf64d11666577171f7dce74a496768449
Arman-Samiei <samiei.arman@gmail.com>
2024-11-26 04:54:18 -0500

    Create permit validator business logic

---
+++ services/permit/src/main/java/ca/mcmaster/cas735/group2/permit/business/PermitValidator.java
@@ -0,0 +1,40 @@
+package ca.mcmaster.cas735.group2.permit.business;
+
+import ca.mcmaster.cas735.group2.permit.business.entities.PermitData;
+import ca.mcmaster.cas735.group2.permit.dto.PermitValidationRequestData;
+import ca.mcmaster.cas735.group2.permit.dto.PermitValidationResponseData;
+import ca.mcmaster.cas735.group2.permit.ports.provided.PermitValidationRequest;
+import ca.mcmaster.cas735.group2.permit.ports.required.PermitRepository;
+import ca.mcmaster.cas735.group2.permit.ports.required.PermitValidationResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@Slf4j
+public class PermitValidator implements PermitValidationRequest {
+    private final PermitRepository database;
+    private final PermitValidationResponse permitValidationResponse;
+
+    @Autowired
+    public PermitValidator(PermitRepository database, PermitValidationResponse permitValidationResponse) {
+        this.database = database;
+        this.permitValidationResponse = permitValidationResponse;
+    }
+
+    @Override
+    public void validate(PermitValidationRequestData permitValidationRequestData) {
+        String transponderID = permitValidationRequestData.getTransponderID();
+        String lotNumber = permitValidationRequestData.getLotNumber();
+        PermitData permitData = database.findByTransponderID(transponderID);
+        PermitValidationResponseData permitValidationResponseData;
+        if (permitData != null && Objects.equals(permitData.getLotNumber(), lotNumber))
+            permitValidationResponseData = new PermitValidationResponseData(true, "The permit is valid");
+        else
+            permitValidationResponseData = new PermitValidationResponseData(false, "The permit is not valid. FUCK OFF!");
+        permitValidationResponse.sendValidationResult(permitValidationResponseData);
+    }
+
+}
