aefc2991e52ef2467b5e986b0cc63bf199b43d97
Arman-Samiei <samiei.arman@gmail.com>
2024-11-28 01:16:29 -0500

    Remove public in interfaces

--- services/permit/src/main/java/ca/mcmaster/cas735/group2/permit/ports/required/PermitRepository.java
+++ services/permit/src/main/java/ca/mcmaster/cas735/group2/permit/ports/required/PermitRepository.java
@@ -4,6 +4,6 @@ import ca.mcmaster.cas735.group2.permit.business.entities.PermitData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermitRepository extends JpaRepository<PermitData, String> {
-    public PermitData findByTransponderID(String transponderID);
-    public PermitData findByPlateNumber(String plateNumber);
+    PermitData findByTransponderID(String transponderID);
+    PermitData findByPlateNumber(String plateNumber);
 }
