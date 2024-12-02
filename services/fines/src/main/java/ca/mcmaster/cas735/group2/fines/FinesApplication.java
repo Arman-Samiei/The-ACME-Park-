a803c21e57973f5f64572dd8e6cb1b34895c8758
Arman-Samiei <samiei.arman@gmail.com>
2024-12-02 12:39:38 -0500

    Create FinesApplication

---
+++ services/fines/src/main/java/ca/mcmaster/cas735/group2/fines/FinesApplication.java
@@ -0,0 +1,12 @@
+package ca.mcmaster.cas735.group2.fines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinesApplication.class, args);
+    }
+}
