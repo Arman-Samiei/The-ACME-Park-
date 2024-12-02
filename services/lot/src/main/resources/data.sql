ff20d3fdf203d099191d78e17c5d76b000e0acb4
Arman-Samiei <samiei.arman@gmail.com>
2024-12-02 02:05:49 -0500

    Use visitor instead of qrVisitor and voucherVisitor as CustomerType

--- services/lot/src/main/resources/data.sql
+++ services/lot/src/main/resources/data.sql
@@ -6,8 +6,8 @@ INSERT INTO spots (SPOT_ID, LOT_ID, CUSTOMER_TYPE, PLATE_NUMBER) VALUES
 ('SPOT104', 'LOT01', 'faculty', NULL),
 ('SPOT105', 'LOT01', 'staff', NULL),
 ('SPOT106', 'LOT01', 'staff', NULL),
-('SPOT107', 'LOT01', 'qrVisitor', NULL),
-('SPOT108', 'LOT01', 'voucherVisitor', NULL),
+('SPOT107', 'LOT01', 'visitor', NULL),
+('SPOT108', 'LOT01', 'visitor', NULL),
 
 -- Lot02 spots
 ('SPOT201', 'LOT02', 'student', NULL),
@@ -16,8 +16,8 @@ INSERT INTO spots (SPOT_ID, LOT_ID, CUSTOMER_TYPE, PLATE_NUMBER) VALUES
 ('SPOT204', 'LOT02', 'faculty', NULL),
 ('SPOT205', 'LOT02', 'staff', NULL),
 ('SPOT206', 'LOT02', 'staff', NULL),
-('SPOT207', 'LOT02', 'qrVisitor', NULL),
-('SPOT208', 'LOT02', 'voucherVisitor', NULL),
+('SPOT207', 'LOT02', 'visitor', NULL),
+('SPOT208', 'LOT02', 'visitor', NULL),
 
 -- Lot03 spots
 ('SPOT301', 'LOT03', 'student', NULL),
@@ -26,8 +26,8 @@ INSERT INTO spots (SPOT_ID, LOT_ID, CUSTOMER_TYPE, PLATE_NUMBER) VALUES
 ('SPOT304', 'LOT03', 'faculty', NULL),
 ('SPOT305', 'LOT03', 'staff', NULL),
 ('SPOT306', 'LOT03', 'staff', NULL),
-('SPOT307', 'LOT03', 'qrVisitor', NULL),
-('SPOT308', 'LOT03', 'voucherVisitor', NULL),
+('SPOT307', 'LOT03', 'visitor', NULL),
+('SPOT308', 'LOT03', 'visitor', NULL),
 
 -- Lot04 spots
 ('SPOT401', 'LOT04', 'student', NULL),
@@ -36,8 +36,8 @@ INSERT INTO spots (SPOT_ID, LOT_ID, CUSTOMER_TYPE, PLATE_NUMBER) VALUES
 ('SPOT404', 'LOT04', 'faculty', NULL),
 ('SPOT405', 'LOT04', 'staff', NULL),
 ('SPOT406', 'LOT04', 'staff', NULL),
-('SPOT407', 'LOT04', 'qrVisitor', NULL),
-('SPOT408', 'LOT04', 'voucherVisitor', NULL),
+('SPOT407', 'LOT04', 'visitor', NULL),
+('SPOT408', 'LOT04', 'visitor', NULL),
 
 -- Lot05 spots
 ('SPOT501', 'LOT05', 'student', NULL),
@@ -46,5 +46,5 @@ INSERT INTO spots (SPOT_ID, LOT_ID, CUSTOMER_TYPE, PLATE_NUMBER) VALUES
 ('SPOT504', 'LOT05', 'faculty', NULL),
 ('SPOT505', 'LOT05', 'staff', NULL),
 ('SPOT506', 'LOT05', 'staff', NULL),
-('SPOT507', 'LOT05', 'qrVisitor', NULL),
-('SPOT508', 'LOT05', 'voucherVisitor', NULL);
+('SPOT507', 'LOT05', 'visitor', NULL),
+('SPOT508', 'LOT05', 'visitor', NULL);
