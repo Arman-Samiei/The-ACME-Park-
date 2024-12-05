686b8ad857b848781c9f4eb5726571477bb31031
Arman-Samiei <samiei.arman@gmail.com>
2024-12-05 16:39:14 -0500

    Set FINES_ID for mock data

--- services/fines/src/main/resources/data.sql
+++ services/fines/src/main/resources/data.sql
@@ -1,16 +1,16 @@
 -- Insert mock data for fines
-INSERT INTO fines (PLATE_NUMBER, AMOUNT)
+INSERT INTO fines (FINES_ID, PLATE_NUMBER, AMOUNT)
 VALUES
 -- Fines for reserved student, faculty, and staff spots
-('DEF456', 50.00), -- Fine for student
-('MNO345', 75.00), -- Fine for faculty
-('VWX234', 30.00), -- Fine for staff
+(1, 'DEF456', 50.00),  -- Fine for student
+(2, 'MNO345', 75.00),  -- Fine for faculty
+(3, 'VWX234', 30.00),  -- Fine for staff
 
 -- Fines for visitor with voucher and occupied spot
-('ABC123', 100.00), -- Fine for visitor with voucher
-('JKL012', 80.00),  -- Fine for visitor with voucher
+(4, 'ABC123', 100.00), -- Fine for visitor with voucher
+(5, 'JKL012', 80.00),  -- Fine for visitor with voucher
 
 -- Fines for visitor without voucher and occupied spot
-('YZA567', 60.00), -- Fine for visitor without voucher
-('HIJ456', 90.00)  -- Fine for visitor without voucher
+(6, 'YZA567', 60.00),  -- Fine for visitor without voucher
+(7, 'HIJ456', 90.00) -- Fine for visitor without voucher
 ;
